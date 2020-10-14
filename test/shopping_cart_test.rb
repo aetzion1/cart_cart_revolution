1572035f69f9916b08d51d0e2777743a1b61f79f
Adam Etzion <adametzion90@gmail.com>
2020-10-14 13:36:57 -0600

    Add shopping cart test corrections

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -47,7 +47,7 @@ class ShoppingCartTest < Minitest::Test
     cart.add_product(product1)
     cart.add_product(product2)
 
-    assert_equal {name: "King Soopers", capacity: 30}, cart.details
+    assert_equal [{name: "King Soopers", capacity: 30}], cart.details
   end
 
 end
