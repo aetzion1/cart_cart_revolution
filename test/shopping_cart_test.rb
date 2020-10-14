239fb37b066491118817ab829117cd71f769409b
Adam Etzion <adametzion90@gmail.com>
2020-10-14 13:55:07 -0600

    Add iteration 3 shopping cart tests

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -51,4 +51,58 @@ class ShoppingCartTest < Minitest::Test
     assert_equal "30", cart.details[:capacity]
   end
 
+  #Iteration3
+  def test_it_it_can_total_number_of_products
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    assert_equal [product1, product2, product3], cart.products
+    assert_equal 13, cart.total_number_of_products
+  end
+
+  def test_it_can_tell_if_full_when_below_capacity
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    refute cart.is_full?
+  end
+
+  def test_it_can_tell_if_full_again_when_above_capacity
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    assert cart.is_full?
+  end
+
+  def test_it_can_list_products_by_specified_category
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    assert_equal [product1, product3], cart.products_by_category(:paper)
+  end
+
 end
