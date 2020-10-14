dfe2e423d0763c7cafe637c10107077a11037401
Adam Etzion <adametzion90@gmail.com>
2020-10-14 13:14:24 -0600

    Add Product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,15 @@
 class Product
   attr_reader :category, :name, :quantity, :unit_price
 
-  def initialize (category, name, quantity, unit_price)
+  def initialize (category, name, unit_price, quantity)
     @category = category
     @name = name
-    @quantity = quantity
+    @quantity = quantity.to_i
     @unit_price = unit_price
   end
 
-
+  def total_price
+    @quantity * @unit_price
+  end
 
 end
